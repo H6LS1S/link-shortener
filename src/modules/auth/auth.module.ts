4303af2efebe799588f1c4a17423a39c48964722
Helsis <seagull.bogdan@gmail.com>
2019-03-16 19:05:07 +0200

    fix(auth): add configuration modules
    Change auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,21 +1,40 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+
+import { ConfigModule } from '../../config/config.module';
+import { ConfigService } from '../../config/config.service';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secretOrPrivateKey: 'secretKey',
-      signOptions: {
-        expiresIn: 3600,
-      },
+		ConfigModule,
+
+    PassportModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        defaultStrategy: configService.getSetting('NEST_DEF_STRATEGY'),
+				session: configService.getSetting('NEST_DEF_SESSION')
+      }),
+      inject: [ConfigService],
     }),
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secretOrPrivateKey: configService.getSetting('JWT_SECRET_KEY'),
+        signOptions: {
+          expiresIn: configService.getSetting('JWT_EXPIRES_TIME'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+
+import { ConfigService } from '../../config/config.service';
 import { JwtPayload } from './interfaces/jwtPayload.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+		private readonly jwtService: JwtService,
+		private readonly configService: ConfigService,
+	) {}
 
   async createToken() {
     const user: JwtPayload = { email: 'test@email.com' };
     const accessToken = this.jwtService.sign(user);
     return {
-      expiresIn: 3600,
+      expiresIn: this.configService.getSetting('JWT_EXPIRES_TIME'),
       accessToken,
     };
   }
