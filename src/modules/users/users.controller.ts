85887409b48c3250ccf8a1edf283df1f1184ab7c
Helsis <seagull.bogdan@gmail.com>
2019-03-17 17:17:26 +0200

    feat(CRUD): add users CRUD
    Change users module

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
+import { Get, Post, Put, Delete } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+	constructor(private readonly usersService: UsersService) {}
+
+	@Post()
+	@UseGuards(AuthGuard())
+	async createUser(): Promise<any> {
+		return await this.usersService.createUser();
+	}
+
+	@Get()
+	@UseGuards(AuthGuard())
+	async selectUser(): Promise<any> {
+		return await this.usersService.selectUser();
+	}
+
+	@Put('setting')
+	@UseGuards(AuthGuard())
+	async updateUser(): Promise<any> {
+		return await this.usersService.updateUser();
+	}
+
+	@Delete('delete')
+	@UseGuards(AuthGuard())
+	async deleteUser(): Promise<any> {
+		return await this.usersService.deleteUser();
+	}
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+  async createUser() {
+    return await 1;
+  }
+
+	async selectUser() {
+		return await 1;
+	}
+
+  async updateUser() {
+    return await 1;
+  }
+
+  async deleteUser() {
+    return await 1;
+  }
+
+}
