74f5c764e8f3ec0aa41e8b8ae26cd3d5498781ce
Helsis <seagull.bogdan@gmail.com>
2019-03-17 18:02:11 +0200

    feat(CRUD): add links CRUD
    Change links module

--- src/modules/links/links.controller.ts
+++ src/modules/links/links.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
+import { Get, Post, Put, Delete } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+import { LinksService } from './links.service';
 
 @Controller('links')
-export class LinksController {}
+export class LinksController {
+	constructor(private readonly linksService: LinksService) {}
+
+	@Post()
+	@UseGuards(AuthGuard())
+	async createPair(): Promise<any> {
+		return await this.linksService.createPair();
+	}
+
+	@Get()
+	@UseGuards(AuthGuard())
+	async selectPair(): Promise<any> {
+		return await this.linksService.selectPair();
+	}
+
+	@Put()
+	@UseGuards(AuthGuard())
+	async updatePair(): Promise<any> {
+		return await this.linksService.updatePair();
+	}
+
+	@Delete()
+	@UseGuards(AuthGuard())
+	async deletePair(): Promise<any> {
+		return await this.linksService.deletePair();
+	}
+}
--- src/modules/links/links.service.ts
+++ src/modules/links/links.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
+import { ConfigService } from '../../config/config.service';
+
 @Injectable()
-export class LinksService {}
+export class LinksService {
+	constructor(
+		private readonly configService: ConfigService,
+	){}
+
+	async createPair() {
+		return await 1;
+	}
+
+	async selectPair() {
+		return await 1;
+	}
+
+	async updatePair() {
+		return await 1;
+	}
+
+	async deletePair() {
+		return await 1;
+	}
+}
